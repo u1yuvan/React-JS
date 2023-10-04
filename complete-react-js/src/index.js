690a28c0409082d71be8fb44949e99ae847b0472
u1yuvan <yuvanraj700@gmail.com>
2023-10-04 13:49:10 +0530

    creating the first component

--- complete-react-js/src/index.js
+++ complete-react-js/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
-const header = <h1>This is my first React App</h1>
+import ReactDOM from 'react-dom/client';
+import App from './app';
+// const header = <h1>This is my first React App</h1>
 
 // console.log(header);
+
+// ReactDOM.render(header, document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>)
