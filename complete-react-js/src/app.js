a4e6fd259aecf79ce166432d75e3119247dd0328
u1yuvan <yuvanraj700@gmail.com>
2023-10-09 20:24:17 +0530

    Passing data via Props

--- complete-react-js/src/app.js
+++ complete-react-js/src/app.js
@@ -1,12 +1,98 @@
 import './app.css';
-import Products from './components/Products';
+import Products from './Components/Products';
+
+const products = [
+  {
+    pID: 1,
+    pName: 'Fresh Milk',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+    isAvailable: true,
+    image: 'images/pngegg (1).png',
+    price: 35
+  },
+  {
+    pID: 2,
+    pName: 'Orange Juice',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+    isAvailable: false,
+    image: "images/pngegg (2).png",
+    price: 50
+  },
+  {
+    pID: 3,
+    pName: 'Pringles',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+    isAvailable: true,
+    image: "images/pngegg (3).png",
+    price: 100
+  },
+  {
+    pID: 4,
+    pName: 'Olive oil',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+    isAvailable: true,
+    image: "images/pngegg (4).png",
+    price: 450
+  },
+  {
+    pID: 5,
+    pName: 'Curd',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+    isAvailable: false,
+    image: "images/pngegg.png",
+    price: 20
+  }
+
+]
 
 function App() {
-    return <div className=' row'>
-      <div className=' col-lg-8 mx-auto'>
-      <Products/>
-      </div>
-    </div>
+  return <div className=' row'>
+    <div className=' col-lg-8 mx-auto'>
+                <Products
+                  id={products[0].pID}
+                  name={products[0].pName}
+                  description={products[0].desc}
+                  isAvailable={products[0].isAvailable}
+                  imageUrl={products[0].image}
+                  price={products[0].price}
+                />
+                <Products
+                  id={products[1].pID}
+                  name={products[1].pName}
+                  description={products[1].desc}
+                  isAvailable={products[1].isAvailable}
+                  imageUrl={products[1].image}
+                  price={products[1].price}
+                />
+
+                <Products
+                  id={products[2].pID}
+                  name={products[2].pName}
+                  description={products[2].desc}
+                  isAvailable={products[2].isAvailable}
+                  imageUrl={products[2].image}
+                  price={products[2].price}
+                />
+
+                <Products
+                  id={products[3].pID}
+                  name={products[3].pName}
+                  description={products[3].desc}
+                  isAvailable={products[3].isAvailable}
+                  imageUrl={products[3].image}
+                  price={products[3].price}
+                />
+
+                <Products
+                  id={products[4].pID}
+                  name={products[4].pName}
+                  description={products[4].desc}
+                  isAvailable={products[4].isAvailable}
+                  imageUrl={products[4].image}
+                  price={products[4].price}
+                />
+              </div>
+            </div>
 }
 
 
--- complete-react-js/src/components/products.js
+++ complete-react-js/src/components/products.js
@@ -1,29 +1,32 @@
 let productCount = 0;
-let imageURL = require("../components/images/pngegg (1).png");
-let isAvailable = 'Unavailable';
+// let imageURL = require("../Components/images/pngegg (1).png");
+// let isAvailable = 'Available';
 
-let badgeClass = 'badge-margin-left-240 badge ';
-badgeClass += isAvailable === 'Available' ? 'bg-success' : 'bg-danger';
 
 
 function displayFormattedProductCount(){
     return productCount > 0 ? productCount : 'Zero';
 }
-function Products() {
+
+
+
+function Products(props) {
+    let badgeClass = 'badge-margin-left-240 badge ';
+badgeClass += props.isAvailable ? 'bg-success' : 'bg-danger';
     return <ul className="list-group shadow">
                 <li className="list-group-item">
                     <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
                         <div className="media-body order-2 order-lg-1">
-                            <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
-                            <p className="font-italic text-muted mb-0 small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.</p>
+                            <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
+                            <p className="font-italic text-muted mb-0 small">{props.description}</p>
                             <div className="d-flex align-items-center justify-content-start mt-1">
-                                <h6 className="font-weight-bold my-2" style= { {'margin-right': 30} }>$120.00</h6>
+                                <h6 className="font-weight-bold my-2" style= { {'margin-right': 30} }>₹{props.price}</h6>
                                 <button className=" btn btn-primary">-</button>
                                 <span style={ {padding: '0px 20px','font-size': 15} }>{displayFormattedProductCount()}</span>
                                 <button className=" btn btn-primary">+</button>
-                                <span className={badgeClass}>{isAvailable}</span>
+                                <span className={badgeClass}>{props.isAvailable ? 'Available' : 'Unavailable'}</span>
                             </div>
-                        </div><img src={imageURL} alt="Generic placeholder image" width="130" class="ml-lg-5 order-1 order-lg-2" />
+                        </div><img src={props.imageUrl} alt="Generic placeholder image" width="100" class="ml-lg-5 order-1 order-lg-2" />
                     </div>
                 </li>
             </ul>
