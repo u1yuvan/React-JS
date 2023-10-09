771f3af23eef3050b1f70fccb517566921b2930b
u1yuvan <yuvanraj700@gmail.com>
2023-10-09 18:48:06 +0530

    Working with JSX attribute

--- complete-react-js/src/components/products.js
+++ complete-react-js/src/components/products.js
@@ -1,4 +1,6 @@
 let productCount = 0;
+let imageURL = require("../components/images/pngegg (1).png");
+
 
 function displayFormattedProductCount(){
     return productCount > 0 ? productCount : 'Zero';
@@ -11,12 +13,12 @@ function Products() {
                             <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
                             <p className="font-italic text-muted mb-0 small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.</p>
                             <div className="d-flex align-items-center justify-content-start mt-1">
-                                <h6 className="font-weight-bold my-2">$120.00</h6>
+                                <h6 className="font-weight-bold my-2" style= { {'margin-right': 30} }>$120.00</h6>
                                 <button className=" btn btn-primary">-</button>
-                                <span>{displayFormattedProductCount()}</span>
+                                <span style={ {padding: '0px 20px','font-size': 15} }>{displayFormattedProductCount()}</span>
                                 <button className=" btn btn-primary">+</button>
                             </div>
-                        </div><img src={require("../components/images/pngegg (1).png")} alt="Generic placeholder image" width="200" class="ml-lg-5 order-1 order-lg-2" />
+                        </div><img src={imageURL} alt="Generic placeholder image" width="200" class="ml-lg-5 order-1 order-lg-2" />
                     </div>
                 </li>
             </ul>
