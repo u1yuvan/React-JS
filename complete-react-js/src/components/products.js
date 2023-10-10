0a1ebb897a392bfe4c944048c14b0ea9d27c40b4
u1yuvan <yuvanraj700@gmail.com>
2023-10-10 13:41:11 +0530

    Splitting Component

--- complete-react-js/src/components/products.js
+++ complete-react-js/src/components/products.js
@@ -1,35 +1,23 @@
-let productCount = 0;
+import ProductDetails from "./ProductDetails";
+
+
 // let imageURL = require("../Components/images/pngegg (1).png");
 // let isAvailable = 'Available';
 
 
 
-function displayFormattedProductCount(){
-    return productCount > 0 ? productCount : 'Zero';
-}
-
-
-
 function Products(props) {
-    let badgeClass = 'badge-margin-left-240 badge ';
-badgeClass += props.isAvailable ? 'bg-success' : 'bg-danger';
-    return <ul className="list-group shadow">
-                <li className="list-group-item">
+   
+    return  <li className="list-group-item">
                     <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
                         <div className="media-body order-2 order-lg-1">
                             <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
                             <p className="font-italic text-muted mb-0 small">{props.description}</p>
-                            <div className="d-flex align-items-center justify-content-start mt-1">
-                                <h6 className="font-weight-bold my-2" style= { {'margin-right': 30} }>₹{props.price}</h6>
-                                <button className=" btn btn-primary">-</button>
-                                <span style={ {padding: '0px 20px','font-size': 15} }>{displayFormattedProductCount()}</span>
-                                <button className=" btn btn-primary">+</button>
-                                <span className={badgeClass}>{props.isAvailable ? 'Available' : 'Unavailable'}</span>
-                            </div>
+                            <ProductDetails price={props.price} isAvailable={props.isAvailable} />
                         </div><img src={props.imageUrl} alt="Generic placeholder image" width="100" class="ml-lg-5 order-1 order-lg-2" />
                     </div>
                 </li>
-            </ul>
+        
 }
 
 
