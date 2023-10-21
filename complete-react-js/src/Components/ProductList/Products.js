bbbe0013e8c73bc83cb944816d0810efd5c96b10
u1yuvan <yuvanraj700@gmail.com>
2023-10-21 12:04:09 +0530

    Dynamic Inline Styling in React

--- complete-react-js/src/Components/ProductList/Products.js
+++ complete-react-js/src/Components/ProductList/Products.js
@@ -10,7 +10,7 @@ import ProductDetails from './ProductDetails';
 
 function Products(props) {
     console.log("Product component executed");
-    return  <li className="list-group-item">
+    return  <li className="list-group-item" style={ {backgroundColor: props.isAvailable ? 'white' : '#dedede'} }>
                     <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
                         <div className="media-body order-2 order-lg-1">
                             <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
